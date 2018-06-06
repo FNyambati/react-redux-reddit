165522b616eae676581f11c31fe3e2de9f009c73
fnyambati <fmnyambati@gmail.com>
2018-06-06 10:40:08 -0600

    fixed Linkedin link, shortened nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,11 @@ const Navbar = ({selectedSub, handleMenuChange, handleRefreshClick}) => (
         <Menu pointing secondary>
             <Menu.Item name='Gaming' active={selectedSub === 'gaming'} onClick={handleMenuChange} />
             <Menu.Item name='funny' active={selectedSub === 'funny'} onClick={handleMenuChange} />
-            <Menu.Item name='todayILearned' active={selectedSub === 'todayILearned'} onClick={handleMenuChange} />
             <Menu.Item name='pics' active={selectedSub === 'pics'} onClick={handleMenuChange} />
             <Menu.Item name='aww' active={selectedSub === 'aww'} onClick={handleMenuChange} />
-            <Menu.Item name='dataIsBeautiful' active={selectedSub === 'dataIsBeautiful'} onClick={handleMenuChange} />
             <Menu.Menu position='right'>
                 <Menu.Item onClick={() => handleRefreshClick()}><Icon name='refresh' /></Menu.Item>
-                <Menu.Item href="http://linkedin.com/fnyambati/" target="_blank"><Icon name='linkedin' size="large" /></Menu.Item>
+                <Menu.Item href="http://linkedin.com/in/fnyambati/" target="_blank"><Icon name='linkedin' size="large" /></Menu.Item>
                 <Menu.Item href="http://github.com/fnyambati/" target="_blank"><Icon name='github' size="large" /></Menu.Item>
             </Menu.Menu>
         </Menu>
